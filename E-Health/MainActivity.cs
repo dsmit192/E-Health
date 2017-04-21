c1fa93b3726bdae56fb926041481454d31f2b03c
Daniel Smith <dsmit192@mail.depaul.edu>
2017-04-21 00:00:28 -0500

    Add project files.

---
+++ E-Health/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace E_Health
+{
+    [Activity(Label = "E_Health", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
