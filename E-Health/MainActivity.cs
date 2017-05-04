95e262f708731c8551c05e4c6c02c2f6a45f43f4
Daniel Smith <dsmit192@mail.depaul.edu>
2017-05-04 00:16:54 -0500

    merged esteban to merge

--- E-Health/MainActivity.cs
+++ E-Health/MainActivity.cs
@@ -33,17 +33,24 @@ namespace E_Health
         {
             base.OnCreate(bundle);
 
-            SetContentView (Resource.Layout.Main);
-            number = 50;
-            submitButton = FindViewById<Button>(Resource.Id.submitButton);
-            submitButton.Click += helpButton_Click;
-        }
-
-        private void helpButton_Click(object sender, EventArgs e)
-        {
-            var intent = new Intent(this, typeof(Results));
-            intent.PutExtra("MyData", number);
-            StartActivity(intent);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            EditText editHeight = FindViewById<EditText>(Resource.Id.HeightInput);
+            EditText editWeight = FindViewById<EditText>(Resource.Id.WeightInput);
+            EditText editBP = FindViewById<EditText>(Resource.Id.BloodPressureInput);
+            EditText editTemp = FindViewById<EditText>(Resource.Id.TemperatureInput);
+            Button SubmitBtn = FindViewById<Button>(Resource.Id.SubmitButton);
+            RatingBar RatingFeeling = FindViewById<RatingBar>(Resource.Id.RatingBar);
+            TextView CheckResult = FindViewById<TextView>(Resource.Id.TESTING);
+            SubmitBtn.Click += (object sender, System.EventArgs e) =>
+            {
+                int n;
+                n = int.Parse(editWeight.Text) +
+                    int.Parse(editTemp.Text) +
+                    int.Parse(editHeight.Text) +
+                    int.Parse(editBP.Text);
+                CheckResult.Text = n + "";
+            };
         }
     }
 }
